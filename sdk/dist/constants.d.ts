5352716098de50d0ef777a0fb1597823199f6238
smexswap <101475047+smexswap@users.noreply.github.com>
2022-04-11 15:44:22 +0530

    router

---
+++ sdk/dist/constants.d.ts
@@ -0,0 +1,43 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | number | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x47b3cB3c0f3A7Cfd484A884C77257B409F2a5Ab2";
+export declare const FACTORY_ADDRESS_MAP: {
+    56: string;
+    97: string;
+};
+export declare const INIT_CODE_HASH = "0x8850004b11400e329532ba54369cf4e29fcc161e6c1acf04b39b5f854832b4ac";
+export declare const INIT_CODE_HASH_MAP: {
+    56: string;
+    97: string;
+};
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
